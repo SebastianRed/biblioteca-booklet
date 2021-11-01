eeab56ad93c47cf66d7eeab5c9c2f36923f3a38f
Sebastián Rojo <sebastianrojocollao@gmail.com>
2021-11-01 18:53:52 -0300

    Commit inicial

---
+++ src/main/java/cl/sebastianrojo/bibliotecabooklet/BibliotecaBookletApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.bibliotecabooklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBookletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBookletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/bibliotecabooklet/BibliotecaBookletApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.bibliotecabooklet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaBookletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
