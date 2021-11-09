e86c15f7dbdead4c2d929cc4f1049e906bc60746
Sebastián Rojo <sebastianrojocollao@gmail.com>
2021-11-08 22:05:14 -0300

    Configura entorno

--- src/main/java/cl/sebastianrojo/bibliotecabooklet/BibliotecaBookletApplication.java
+++ src/main/java/cl/sebastianrojo/bibliotecabooklet/BibliotecaBookletApplication.java
@@ -2,12 +2,17 @@ package cl.sebastianrojo.bibliotecabooklet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import cl.sebastianrojo.bibliotecabooklet.service.ILibroService;
 
 @SpringBootApplication
 public class BibliotecaBookletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaBookletApplication.class, args);
+		//SpringApplication.run(BibliotecaBookletApplication.class, args);
+		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext(AppConfig.class);
+		ILibroService libroService = acac.getBean(ILibroService.class);
+		System.out.println(libroService.findAll());
 	}
-
 }
